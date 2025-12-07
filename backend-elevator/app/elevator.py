12154ea0716a2ec21ce097dfd9b4d53aa184445e
ticiano.damasceno@hotmail.com <ticiano.damasceno@hotmail.com>
2025-12-07 17:18:30 -0300

    2: criando backend-elevator

---
+++ backend-elevator/app/elevator.py
@@ -0,0 +1,73 @@
+import asyncio
+from pydantic import BaseModel
+from typing import Literal, List, Optional
+
+Status = Literal['subindo', 'descendo', 'parado']
+
+class ElevatorState(BaseModel):
+    status: Status = 'parado'
+    localidade: int = 0
+
+
+class Elevator:
+    def __init__(self) -> None:
+        self.state = ElevatorState()
+        self._lock = asyncio.Lock()
+        self.calls: List[int] = []
+        self._running = False
+
+    def add_call(self, floor: int) -> None:
+        async with self._lock:
+            if floor not in self.calls and floor != self.state.localidade:
+                self.calls.append(floor)
+            if floor == self.state.localidade and self.state.status == 'parado':
+                print('solicita abertura da porta')
+
+    async def up(self):
+        if self.state.localidade == 7:
+            return
+        self.state.status = 'subindo'
+        print('subindo para o andar', self.state.localidade + 1)
+        await asyncio.sleep(3)
+        self.state.localidade += 1
+    
+    async def down(self):
+        if self.state.localidade == 0:
+            return
+        self.state.status = 'descendo'
+        print('descendo para o andar', self.state.localidade - 1)
+        await asyncio.sleep(3)
+        self.state.localidade -= 1
+
+    def _next_near_floor(self) -> Optional[int]:
+        if not self.calls:
+            return None
+        return min(self.calls, key = lambda x: abs(x - self.state.localidade))
+
+    async def run(self, on_stop_callback):
+        if self._running:
+            return
+        self._running = True
+        while self.calls:
+            async with self._lock:
+                next_floor = self._next_near_floor()
+            if next_floor is None:
+                break
+
+            if next_floor == self.state.localidade:
+                self.calls.remove(next_floor)
+                self.state.status = 'parado'
+                await on_stop_callback(self.state.localidade)
+                continue
+
+            if next_floor > self.state.localidade:
+                await self.up()
+            elif next_floor < self.state.localidade:
+                await self.down()
+            if self.state.localidade in self.calls:
+                self.calls.remove(self.state.localidade)
+                self.state.status = 'parado'
+                await on_stop_callback(self.state.localidade)
+
+        self.state.status = 'parado'
+        self._running = False
---
+++ backend-elevator/app/main.py
@@ -0,0 +1,104 @@
+import os
+import asyncio
+import json
+import redis.asyncio as redis
+from fastapi import FastAPI, WebSocket, HTTPException
+from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+
+
+from elevator import Elevator
+
+load_dotenv()
+REDIS_URL = os.getenv("REDIS_URL", "redis://172.17.144.1:6379")
+
+ELEVATOR_CHANNEL = "elevator:events"
+DOORS_COMMAND_CHANNEL = "doors:commands"
+
+app = FastAPI(title = 'Elevator service')
+elevator = Elevator()
+
+async def get_redis():
+    return redis.from_url(REDIS_URL, decode_responses=True)
+
+async def publish(channel: str, message: dict|str):
+    r = await get_redis()
+    try:
+        if isinstance(message, dict):
+            message = json.dumps(message)
+        await r.publish(channel, message)
+    finally:
+        await r.close()
+
+async def on_elevator_stop(floor:int):
+    await publish(
+        ELEVATOR_CHANNEL,
+        {
+            'type': 'stop',
+            'localidade': floor,
+            'status': 'parado'
+        }
+    )
+
+    await publish(DOORS_COMMAND_CHANNEL, f'open:{floor}')
+
+@app.post('/call/{floor}')
+async def call_elevator(floor: int):
+    if not 0 <= floor <= 7:
+        raise HTTPException(
+            status_code=400,
+            detail='Andar ser um inteiro entre 0 e 7'
+        )
+    
+    await elevator.add_call(floor)
+
+    await publish(ELEVATOR_CHANNEL, {
+        'type': 'queue',
+        'calls': elevator.calls
+    })
+
+    asyncio.create_task(elevator.run(on_elevator_stop))
+    # await elevator.run(on_elevator_stop)
+
+    return JSONResponse({
+            'OK': True,
+            'fila': elevator.calls,
+            'localidade_atual': elevator.state.localidade
+    })
+
+@app.get('/status')
+async def status():
+    return {
+        'localidade': elevator.state.localidade,
+        'status': elevator.state.status,
+        'fila': elevator.calls
+    }
+
+@app.websocket('/ws')
+async def websocket_endpoint(ws: WebSocket):
+    await ws.accept()
+    
+    r = await get_redis()
+    pubsub = r.pubsub()
+    await pubsub.subscribe(ELEVATOR_CHANNEL)
+    
+    try:
+        while True:
+            message = await pubsub.get_message(
+                ignore_subscribe_messages=True, 
+                timeout = 1.0
+            )
+            
+            if message:
+                await ws.send_text(str(message['data']))
+            
+            try:
+                await asyncio.wait_for(ws.receive_text(), timeout=0.1)
+            except asyncio.TimeoutError:
+                pass
+    
+    finally:
+        await pubsub.unsubscribe(ELEVATOR_CHANNEL)
+        await r.close()
+        await ws.close()
+            
